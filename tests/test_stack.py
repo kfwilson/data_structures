f1c010f0be9a6feda54be15f333a57e40ea32ff2
kfwilson <kwilson1391@gmail.com>
2018-05-11 12:50:42 -0700

    updating handling of oversized list to constructor updating test stack

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -4,20 +4,21 @@ import pytest
 
 from stack.stack import Stack, StackOverflowError
 
-_orderable_lists = [list(range(15)), [c for c in string.ascii_letters], [1, 3, 1.1, -50, 0, 3.1415639]]
-_unorderable_lists = [['banana', 50, False], [1, 2, 3, None],
-                      [c for c in string.ascii_letters] + list(range(10))]
-_constructor_args_non_empty = _unorderable_lists + _orderable_lists
-_constructor_args_with_empty = _unorderable_lists + _orderable_lists + [[]]
+_lists = [list(range(15)), 
+          [c for c in string.ascii_letters], 
+          [5, 22.35, -15.26845, -1, .0025],
+          ['words', 235, False], [None, 'a', 1, 5],
+          [c for c in string.ascii_letters] + list(range(15))]
+_lists_with_empty = _lists + [[]]
 
 
-@pytest.fixture(params=_constructor_args_with_empty, ids=repr)
-def constructor_arg(request):
+@pytest.fixture(params=_lists_with_empty, ids=repr)
+def list_arg(request):
     return request.param
 
 
-@pytest.fixture(params=_constructor_args_non_empty, ids=repr)
-def constructor_arg_non_empty(request):
+@pytest.fixture(params=_lists, ids=repr)
+def list_arg_non_empty(request):
     return request.param
 
 
@@ -26,15 +27,15 @@ def test_stack_empty_constructor():
     assert st.head is None
 
 
-def test_push(constructor_arg):
-    st = Stack(constructor_arg)
+def test_push(list_arg):
+    st = Stack(list_arg)
     st.push('test')
     assert st.pop() == 'test'
 
 
-def test_constructor_list_order(constructor_arg_non_empty):
-    st = Stack(constructor_arg_non_empty)
-    assert st.pop() == constructor_arg_non_empty[-1]
+def test_constructor_list_order(list_arg_non_empty):
+    st = Stack(list_arg_non_empty)
+    assert st.pop() == list_arg_non_empty[-1]
 
 
 def test_pop_empty_stack():
@@ -47,18 +48,18 @@ def test_peek_empty_stack():
         Stack().peek()
 
 
-def test_stack_constructor_oversize_error(constructor_arg):
+def test_stack_constructor_oversize_error(list_arg):
     with pytest.raises(StackOverflowError):
-        Stack(constructor_arg, len(constructor_arg)-1)
+        Stack(list_arg, len(list_arg)-1)
 
 
-def test_push_oversize(constructor_arg):
-    st = Stack(constructor_arg, len(constructor_arg))
+def test_push_oversize(list_arg):
+    st = Stack(list_arg, len(list_arg))
     with pytest.raises(StackOverflowError):
         st.push('one too many')
 
 
-def test_peek_leaves_element_on_stack(constructor_arg_non_empty):
-    st = Stack(constructor_arg_non_empty)
+def test_peek_leaves_element_on_stack(list_arg_non_empty):
+    st = Stack(list_arg_non_empty)
     st.peek()
-    assert st.peek() == constructor_arg_non_empty[-1]
+    assert st.peek() == list_arg_non_empty[-1]
