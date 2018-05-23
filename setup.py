b013b77234eb9b0a2563940a287659cc4f084963
kfwilson <kwilson1391@gmail.com>
2018-05-22 20:34:59 -0700

    updating package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='data_structures',
+    version='',
+    packages=find_packages(where='src'),
+    package_dir={'':'src'},
+    include_pacakge_data=TRUE,
+    install_Requies=['pytest',]
+    author='kwilson',
+)
